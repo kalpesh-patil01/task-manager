0731bf6a605bd0224f8206029529824e385b2fb0
kalpesh-patil01 <kalpeshpatillenovo@gmail.com>
2026-05-11 22:56:20 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    # SocketIO vaprun app run kara (Real-time features sathi)
+    # wth the help of socketio run app 
     socketio.run(app, debug=True)
